0e40dadb7483e71c02c8ff18dade58bb16c148f9
Tuhin Ahmed <117445766+JessicamMnQShar@users.noreply.github.com>
2023-02-12 20:38:25 +0600

    Update tuhin.py

--- tuhin.py
+++ tuhin.py
@@ -1,6 +1,6 @@
 
 import os,sys,time,json,random,re,string,platform,base64,uuid
-from bs4 import BeautifulSoup as sop
+from 'bs4' import BeautifulSoup as sop
 from bs4 import BeautifulSoup
 import requests as ress
 from datetime import date
